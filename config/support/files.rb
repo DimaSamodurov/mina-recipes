2863d7e2780ae46af327de51cb8c762e21bdfcfb
Dmytro Samodurov <dimasamodurov@gmail.com>
2018-10-01 04:15:46 +0300

    Simplify use of templates.

--- config/support/files.rb
+++ config/support/files.rb
@@ -1,5 +1,16 @@
 def remote_file(file_path, content: nil)
-  body = content || yield
-  raise ':content argument or block value is expected.' if body.nil?
+  body = content
+  body ||= yield if block_given?
+  template_path = default_template_path(file_path)
+  body ||= erb(template_path) if template_path
+  raise ":content argument, block value or default template is expected for #{file_path}." if body.nil?
   command %(echo #{Shellwords.escape body} | sudo tee #{file_path})
+end
+
+def default_template_path(remote_file_path)
+  template_name = remote_file_path.tr('/', '_')
+  mask = File.join(__dir__, '..', '**/templates/**/*')
+  files = Dir[mask]
+  template_path = files.detect { |path| File.basename(path) == template_name }
+  File.realpath template_path if template_path
 end
