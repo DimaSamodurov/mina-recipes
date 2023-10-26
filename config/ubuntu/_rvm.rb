60d92572b84b878d4619142ebf4e2def9311db45
Dmytro Samodurov <dima@EPUALVIW07F4>
2023-10-26 22:23:28 +0300

    Update RVM install

--- config/ubuntu/_rvm.rb
+++ config/ubuntu/_rvm.rb
@@ -3,13 +3,11 @@ namespace :ubuntu do
     desc 'Install RVM'
     task :install do
       command <<~BASH
-        sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+        sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev gnupg2
       BASH
 
       command <<~BASH
-        gpg --keyserver hkp://keys.gnupg.net --recv-keys \
-          409B6B1796C275462A1703113804BB82D39DC0E3 \
-          7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+        gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
         curl -sSL https://get.rvm.io | bash -s stable
         source ~/.rvm/scripts/rvm
         rvm info
